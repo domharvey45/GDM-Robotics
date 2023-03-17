c3a90ed0594dcff71a5dba01f734a0de4a9d96ce
RubenHeyse <ruben.heyse@icloud.com>
2023-03-17 22:42:39 +0000

    feat(commitlint): modify commitlint config
    modify commitlint config to add rules and prompt

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,39 @@
+// CONFIG DOCS: https://commitlint.js.org/#/reference-configuration
+
+
+// MESSAGE ANATOMY (? = optional):
+//
+// type(scope?): subject
+// body?
+// footer?
+
+
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	parserPreset: 'conventional-changelog-conventionalcommits',
+	helpUrl: 'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+
+	// RULES DOCS: https://commitlint.js.org/#/reference-rules
 	rules: {
+
+		// HEADER
+		'header-max-length': [2, 'always', 100],
+		'header-case': [2, 'always', 'lower-case'],
+
+		// BODY
 		'body-leading-blank': [1, 'always'],
 		'body-max-line-length': [2, 'always', 100],
+
+		// FOOTER
 		'footer-leading-blank': [1, 'always'],
 		'footer-max-line-length': [2, 'always', 100],
-		'header-max-length': [2, 'always', 100],
+
+		//SCOPE
+		'scope-case': [2, 'always', 'lower-case'],
+		'scope-empty': [1, 'never'],
+		'scope-max-length': [2, 'always', 20],
+
+		// SUBJECT
 		'subject-case': [
 			2,
 			'never',
@@ -14,6 +41,8 @@ module.exports = {
 		],
 		'subject-empty': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
+
+		//TYPE
 		'type-case': [2, 'always', 'lower-case'],
 		'type-empty': [2, 'never'],
 		'type-enum': [
@@ -35,103 +64,97 @@ module.exports = {
 		],
 	},
 	prompt: {
-		questions: {
-			type: {
-				description: "Select the type of change that you're committing",
-				enum: {
-					feat: {
-						description: 'A new feature',
-						title: 'Features',
-
-					},
-					fix: {
-						description: 'A bug fix',
-						title: 'Bug Fixes',
-
-					},
-					docs: {
-						description: 'Documentation only changes',
-						title: 'Documentation',
-
-					},
-					style: {
-						description:
-							'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-						title: 'Styles',
-
-					},
-					refactor: {
-						description:
-							'A code change that neither fixes a bug nor adds a feature',
-						title: 'Code Refactoring',
-
-					},
-					perf: {
-						description: 'A code change that improves performance',
-						title: 'Performance Improvements',
-
-					},
-					test: {
-						description: 'Adding missing tests or correcting existing tests',
-						title: 'Tests',
-
-					},
-					build: {
-						description:
-							'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
-						title: 'Builds',
-
-					},
-					ci: {
-						description:
-							'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
-						title: 'Continuous Integrations',
-
-					},
-					chore: {
-						description: "Other changes that don't modify src or test files",
-						title: 'Chores',
-
-					},
-					revert: {
-						description: 'Reverts a previous commit',
-						title: 'Reverts',
-
-					},
-				},
-			},
-			scope: {
-				description:
-					'What is the scope of this change (e.g. component or file name)',
-			},
-			subject: {
-				description:
-					'Write a short, imperative tense description of the change',
-			},
-			body: {
-				description: 'Provide a longer description of the change',
-			},
-			isBreaking: {
-				description: 'Are there any breaking changes?',
-			},
-			breakingBody: {
-				description:
-					'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
-			},
-			breaking: {
-				description: 'Describe the breaking changes',
-			},
-			isIssueAffected: {
-				description: 'Does this change affect any open issues?',
-			},
-			issuesBody: {
-				description:
-					'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
-			},
-			issues: {
-				description: 'Add issue references (e.g. "fix #123", "re #123".)',
-			},
-		},
-	},
+    settings: {},
+    messages: {
+      skip: ':skip',
+      max: 'upper %d chars',
+      min: '%d chars at least',
+      emptyWarning: 'can not be empty',
+      upperLimitWarning: 'over limit',
+      lowerLimitWarning: 'below limit'
+    },
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing:",
+        enum: {
+          feat: {
+            description: 'A new feature',
+            title: 'Features',
+            emoji: '✨',
+          },
+          fix: {
+            description: 'A bug fix',
+            title: 'Bug Fixes',
+            emoji: '🐛',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📚',
+          },
+          style: {
+            description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+            title: 'Styles',
+            emoji: '💎',
+          },
+          refactor: {
+            description: 'A code change that neither fixes a bug nor adds a feature',
+            title: 'Code Refactoring',
+            emoji: '📦',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            title: 'Performance Improvements',
+            emoji: '🚀',
+          },
+          test: {
+            description: 'Adding missing tests or correcting existing tests',
+            title: 'Tests',
+            emoji: '🚨',
+          },
+          build: {
+            description: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+            title: 'Builds',
+            emoji: '🛠',
+          },
+          ci: {
+            description: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+            title: 'Continuous Integrations',
+            emoji: '⚙️',
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: 'Chores',
+            emoji: '♻️',
+          },
+          revert: {
+            description: 'Reverts a previous commit',
+            title: 'Reverts',
+            emoji: '🗑',
+          },
+        },
+      },
+      scope: {
+        description:
+          'What is the scope of this change (e.g. component or file name)',
+      },
+      subject: {
+        description: 'Write a short, imperative tense description of the change',
+      },
+      body: {
+        description: 'Provide a longer description of the change',
+      },
+      isBreaking: {
+        description: 'Are there any breaking changes?',
+      },
+      breakingBody: {
+        description:
+          'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
+      },
+      breaking: {
+        description: 'Describe the breaking changes',
+      }
+    },
+  }
 };
 
